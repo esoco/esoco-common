4e3c25f5888c9d0518a0221064b215811261d9b8
Elmar Sonnenschein <eso@esoco.de>
2016-05-11 18:11:40 +0200

    FRAMEWORK-180 (Bug): Integrate the GWT Material Design project with the framework

--- src/main/java/de/esoco/lib/property/UserInterfaceProperties.java
+++ src/main/java/de/esoco/lib/property/UserInterfaceProperties.java
@@ -116,37 +116,43 @@ public class UserInterfaceProperties
 	public enum LabelStyle { DEFAULT, INLINE, FORM, TITLE, BRAND, ICON }
 
 	/********************************************************************
-	 * Enumeration of the display modes for data element lists that define how
-	 * data elements will be arranged in the generated user interface. The
-	 * possible values are:
+	 * Enumeration of the layouts for UI element containers. The layout defines
+	 * how elements will be arranged in the generated user interface. Not all
+	 * layouts may be supported by an implementation but in that case it should
+	 * fall back to a reasonable default placement of elements. The possible
+	 * values are:
 	 *
 	 * <ul>
 	 *   <li>{@link #TABLE}: Data elements are placed in the cells of a
-	 *     table-like structure (HTML: table).</li>
-	 *   <li>{@link #DOCK}: Elements are arranged arround the edges of a center
-	 *     element (HTML: divs with the center at 100% size). The size and
-	 *     orientation (horizontal or vertical) of the surrounding must be set
-	 *     as UI properties.</li>
+	 *     table-like structure.</li>
+	 *   <li>{@link #DOCK}: Elements are arranged around the edges of a center
+	 *     element. The size and orientation (horizontal or vertical) of the
+	 *     surrounding must be set as UI properties.</li>
 	 *   <li>{@link #SPLIT}: Like {@link #DOCK} but with resizable side areas.
 	 *   </li>
 	 *   <li>{@link #TABS}: A panel with selectable tabs for each contained data
-	 *     element (HTML: full size div).</li>
+	 *     element.</li>
 	 *   <li>{@link #STACK}: Like {@link #TABS} but arranged as a vertical stack
 	 *     of collapsing stacks for each element child.</li>
 	 *   <li>{@link #DECK}: Like {@link #TABS} but without an UI for selecting
 	 *     child elements. Selection must occur programmatically.</li>
-	 *   <li>{@link #FILL}: A single UI elements fills the available area (HTML:
-	 *     div with 100% size).</li>
+	 *   <li>{@link #FILL}: A single UI elements fills the available area.</li>
 	 *   <li>{@link #FLOW}: UI elements flow in the natural order defined by the
-	 *     UI context (HTML: div).</li>
-	 *   <li>{@link #GRID}: Like FLOW but elements are automatically arranged
-	 *     according to their properties like in a {@link #FORM} (HTML: div with
-	 *     a CSS grid layout).</li>
-	 *   <li>{@link #FORM}: Arranges data elements according to their properties
-	 *     in an input form (HTML: form).</li>
-	 *   <li>{@link #GROUP}: Arranges data elements in a distinctive group
-	 *     (HTML: fieldset).</li>
-	 *   <li>{@link #MENU}: A menu or navigation structure (HTML: nav).</li>
+	 *     UI context.</li>
+	 *   <li>{@link #GRID}: Like FLOW but elements are placed in a grid
+	 *     structure according to their properties.</li>
+	 *   <li>{@link #LIST}: Places elements in a list.</li>
+	 *   <li>{@link #LIST_ITEM}: A single element in a {@link #LIST}
+	 *     layout.</li>
+	 *   <li>{@link #CARD}: A structured card with specific elements like title,
+	 *     content, actions.</li>
+	 *   <li>{@link #FORM}: Arranges elements according to their properties like
+	 *     in a {@link #GRID} but in some implementations (e.g. HTML-based) with
+	 *     a structure that defines an explicit input form.</li>
+	 *   <li>{@link #GROUP}: Arranges elements in a distinctive group, typically
+	 *     inside a {@link #FORM}.</li>
+	 *   <li>{@link #MENU}: A menu or some other kind of navigation
+	 *     structure.</li>
 	 *   <li>{@link #HEADER}: The header area of an application.</li>
 	 *   <li>{@link #CONTENT}: The main content area of an application.</li>
 	 *   <li>{@link #FOOTER}: The footer area of an application.</li>
@@ -156,8 +162,8 @@ public class UserInterfaceProperties
 	 */
 	public enum Layout
 	{
-		TABLE, DOCK, SPLIT, TABS, STACK, DECK, FILL, FLOW, GRID, FORM, GROUP,
-		MENU, HEADER, CONTENT, FOOTER, INLINE
+		TABLE, DOCK, SPLIT, TABS, STACK, DECK, FILL, FLOW, GRID, LIST,
+		LIST_ITEM, CARD, FORM, GROUP, MENU, HEADER, CONTENT, FOOTER, INLINE
 	}
 
 	/********************************************************************
