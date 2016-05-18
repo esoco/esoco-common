f5efe381d8e4e847e6847b11e928a31542b4529d
Elmar Sonnenschein <eso@esoco.de>
2016-05-18 18:58:18 +0200

    FRAMEWORK-183 (Sub-task): GEWT: add support for icons to components

--- src/main/java/de/esoco/lib/property/ButtonStyle.java
+++ src/main/java/de/esoco/lib/property/ButtonStyle.java
@@ -1,3 +1,19 @@
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+// This file is a part of the 'esoco-common' project.
+// Copyright 2016 Elmar Sonnenschein, esoco GmbH, Flensburg, Germany
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//	  http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 package de.esoco.lib.property;
 
 /********************************************************************
@@ -11,6 +27,7 @@ package de.esoco.lib.property;
  *   <li>{@link #FLAT}: A flat display style.</li>
  *   <li>{@link #FLOAT}: Display button over other content.</li>
  *   <li>{@link #LINK}: Display buttons as (hyper-) links.</li>
+ *   <li>{@link #LINK}: Display buttons as an icon (without text).</li>
  * </ul>
  */
-public enum ButtonStyle { DEFAULT, OUTLINE, FLAT, FLOAT, LINK }
+public enum ButtonStyle { DEFAULT, OUTLINE, FLAT, FLOAT, LINK, ICON }
