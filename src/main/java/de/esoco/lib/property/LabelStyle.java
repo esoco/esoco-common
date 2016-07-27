1d9a398facd8102e5e56d2486de919b72cb5c978
Elmar Sonnenschein <eso@esoco.de>
2016-07-27 12:24:13 +0200

    NEW: image labels OPT: icon handling

--- src/main/java/de/esoco/lib/property/LabelStyle.java
+++ src/main/java/de/esoco/lib/property/LabelStyle.java
@@ -1,21 +1,38 @@
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+// This file is a part of the 'esoco-common' project.
+// Copyright 2016 Elmar Sonnenschein, esoco GmbH, Flensburg, Germany
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//	  http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 package de.esoco.lib.property;
 
 /********************************************************************
- * An enumeration of the styles for displaying UI labels. Not all styles may
- * be supported by an UI implementation in which case it should revert to a
+ * An enumeration of the styles for displaying UI labels. Not all styles may be
+ * supported by an UI implementation in which case it should revert to a
  * reasonable default if possible.
  *
  * <ul>
  *   <li>{@link #DEFAULT}: The default label style.</li>
- *   <li>{@link #INLINE}: A label that should explicitly be displayed
- *     in-line with other UI elements.</li>
- *   <li>{@link #FORM}: A label that serves as a label for another UI
- *     element in a form panel.</li>
+ *   <li>{@link #INLINE}: A label that should explicitly be displayed in-line
+ *     with other UI elements.</li>
+ *   <li>{@link #FORM}: A label that serves as a label for another UI element in
+ *     a form panel.</li>
  *   <li>{@link #TITLE}: A title for other UI elements.</li>
- *   <li>{@link #BRAND}: A brand description, typically in the main
- *     navigation menu</li>
+ *   <li>{@link #BRAND}: A brand description, typically in the main navigation
+ *     menu</li>
  *   <li>{@link #ICON}: The label text will be mapped to a corresponding
  *     icon.</li>
+ *   <li>{@link #IMAGE}: The label text references an image file.</li>
  * </ul>
  */
-public enum LabelStyle { DEFAULT, INLINE, FORM, TITLE, BRAND, ICON }
+public enum LabelStyle { DEFAULT, INLINE, FORM, TITLE, BRAND, ICON, IMAGE }
