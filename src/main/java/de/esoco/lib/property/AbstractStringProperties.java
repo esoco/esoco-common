6cbd1cef01e8913422a81e30ecf44c2ecda83239
Elmar Sonnenschein <eso@esoco.de>
2017-07-16 20:51:54 +0200

    OPT: toString()

--- src/main/java/de/esoco/lib/property/AbstractStringProperties.java
+++ src/main/java/de/esoco/lib/property/AbstractStringProperties.java
@@ -1,6 +1,6 @@
 //++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 // This file is a part of the 'esoco-common' project.
-// Copyright 2016 Elmar Sonnenschein, esoco GmbH, Flensburg, Germany
+// Copyright 2017 Elmar Sonnenschein, esoco GmbH, Flensburg, Germany
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -189,7 +189,7 @@ public abstract class AbstractStringProperties implements HasProperties,
 	@Override
 	public String toString()
 	{
-		return "StringProperties" +
+		return getClass().getSimpleName() +
 			   (aPropertyMap != null ? aPropertyMap : "[]");
 	}
 
