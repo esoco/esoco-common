912daed662c381def1bc0f617af427e81da48e7f
Elmar Sonnenschein <eso@esoco.de>
2017-08-17 17:03:03 +0200

    framework/esoco-business#23: UI API: implement paging table list

--- src/main/java/de/esoco/lib/property/Updatable.java
+++ src/main/java/de/esoco/lib/property/Updatable.java
@@ -1,6 +1,6 @@
 //++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 // This file is a part of the 'esoco-common' project.
-// Copyright 2015 Elmar Sonnenschein, esoco GmbH, Flensburg, Germany
+// Copyright 2017 Elmar Sonnenschein, esoco GmbH, Flensburg, Germany
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -22,6 +22,7 @@ package de.esoco.lib.property;
  *
  * @author eso
  */
+@FunctionalInterface
 public interface Updatable
 {
 	//~ Methods ----------------------------------------------------------------
