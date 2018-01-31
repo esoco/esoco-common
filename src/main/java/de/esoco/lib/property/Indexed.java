b5f87def29f3d439410abd24b5ad8b55fada7506
Elmar Sonnenschein <eso@esoco.de>
2018-01-31 09:22:15 +0100

    NEW: Indexed interface

---
+++ src/main/java/de/esoco/lib/property/Indexed.java
@@ -0,0 +1,34 @@
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+// This file is a part of the 'esoco-common' project.
+// Copyright 2018 Elmar Sonnenschein, esoco GmbH, Flensburg, Germany
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//	  http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+package de.esoco.lib.property;
+
+/********************************************************************
+ * An interface for objects that are enumerated with an integer index.
+ *
+ * @author eso
+ */
+public interface Indexed
+{
+	//~ Methods ----------------------------------------------------------------
+
+	/***************************************
+	 * Returns the index value for this object.
+	 *
+	 * @return The object's index
+	 */
+	public int getIndex();
+}
