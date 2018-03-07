8eb8d2e7ae45857e0823f6451a12a4d7ef261416
Elmar Sonnenschein <eso@esoco.de>
2018-03-07 18:04:21 +0100

    esoco/sdack#31: UI: introduce content type for HTML text values

--- src/main/java/de/esoco/lib/property/ContentType.java
+++ src/main/java/de/esoco/lib/property/ContentType.java
@@ -1,12 +1,29 @@
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+// This file is a part of the 'esoco-common' project.
+// Copyright 2018 Elmar Sonnenschein, esoco GmbH, Flensburg, Germany
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//	  http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 package de.esoco.lib.property;
 
 /********************************************************************
- * An indication of the type of content stored by an element that will
- * influence the rendering of the content. What exactly that means and which
- * content types are supported depends on the respective element
- * implementation. The following types are available:
+ * An indication of the type of content stored by an element that will influence
+ * the rendering of the content. What exactly that means and which content types
+ * are supported depends on the respective element implementation. The following
+ * types are available:
  *
  * <ul>
+ *   <li>{@link #HTML}: HTML code that should be rendered as such.</li>
  *   <li>{@link #PHONE_NUMBER}: a phone number.</li>
  *   <li>{@link #PASSWORD}: a password or other sensitive information which
  *     should not be visible.</li>
@@ -16,9 +33,8 @@ package de.esoco.lib.property;
  *     displayed.</li>
  *   <li>{@link #WEBSITE}: a URL to be displayed in-line.</li>
  *   <li>{@link #HYPERLINK}: a URI to be visualized as a hyperlink.</li>
- *   <li>{@link #RELATIVE_URL}: a URL which is relative to the current
- *     context (and therefore possibly needs to be expanded into an absolute
- *     URL).</li>
+ *   <li>{@link #RELATIVE_URL}: a URL which is relative to the current context
+ *     (and therefore possibly needs to be expanded into an absolute URL).</li>
  *   <li>{@link #ABSOLUTE_URL}: a URL which is independent of the current
  *     context.</li>
  *   <li>{@link #FILE_UPLOAD}: the content will be created from a file
@@ -27,6 +43,6 @@ package de.esoco.lib.property;
  */
 public enum ContentType
 {
-	PHONE_NUMBER, PASSWORD, PROGRESS, DATE_TIME, WEBSITE, HYPERLINK,
+	HTML, PHONE_NUMBER, PASSWORD, PROGRESS, DATE_TIME, WEBSITE, HYPERLINK,
 	RELATIVE_URL, ABSOLUTE_URL, FILE_UPLOAD
 }
