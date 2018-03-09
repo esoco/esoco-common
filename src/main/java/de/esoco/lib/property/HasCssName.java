704ceda4e6d9d8df1aa8b01b55dd35a001e8a784
Elmar Sonnenschein <eso@esoco.de>
2018-03-09 16:05:38 +0100

    esoco/sdack#28: UI: add support for flex layouts
    NEW: HasCssName

---
+++ src/main/java/de/esoco/lib/property/HasCssName.java
@@ -0,0 +1,35 @@
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+// This file is a part of the 'esoco-common' project.
+// Copyright 2018 Elmar Sonnenschein, esoco GmbH, Flensburg, Germany
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//	  http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+package de.esoco.lib.property;
+
+/********************************************************************
+ * A property interface for objects that provide a text representation for use
+ * in CSS.
+ *
+ * @author eso
+ */
+public interface HasCssName
+{
+	//~ Methods ----------------------------------------------------------------
+
+	/***************************************
+	 * Gets the CSS name of this object.
+	 *
+	 * @return The CSS name string
+	 */
+	String getCssName();
+}
