38d50fc4bf9c56f7fa361816f4de7bd8ebfa2e08
Elmar Sonnenschein <eso@esoco.de>
2018-04-07 21:37:35 +0200

    NEW: ErrorHandling enum

---
+++ src/main/java/de/esoco/lib/property/ErrorHandling.java
@@ -0,0 +1,7 @@
+package de.esoco.lib.property;
+
+/********************************************************************
+ * A generic enumeration of error handling options that can bet set on
+ * processing or processed objects as meta information.
+ */
+public enum ErrorHandling { THROW, LOG, IGNORE }
